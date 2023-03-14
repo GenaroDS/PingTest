9c706e5cb767dc709bc291234f0f71ddffe4fa9d
GenaroDS <genarodistilio@gmail.com>
2023-03-14 19:15:22 -0300

    This version has better error handeling, and runs one more thread for the settings window, the code has been cleaned up to

--- PackageTester.py
+++ PackageTester.py
@@ -8,15 +8,16 @@ import sys
 import configparser
 import pystray
 from PIL import Image
-# Settings change flag for ping_test
-settings_changed = False
+
+# Set flags for ping_test
+settings_changed = True          
+stop_ping_test = False
 
 #Open settings file
 def open_settings():
-    try:
-        os.startfile('Settings.txt')
-    except OSError:
-        print('Error: Could not open file')
+    global settings_changed
+    subprocess.run(["notepad.exe", "Settings.txt"])
+    settings_changed = True
 
 # Change system tray icon
 def icon_change(name):
@@ -24,65 +25,81 @@ def icon_change(name):
     image = Image.open(icon_path)
     tray_icon.icon = image
 
-# Handle right click on the system tray icon
+# Stop ping test and icon main thread thread when click on exit
 def on_right_click(icon, item):
         global stop_ping_test
         stop_ping_test = True
         icon.stop()
 
+#Create a third thread when settings is opened
 def on_right_click_settings(icon, item):        
-        global settings_changed
-        settings_changed = True
-        open_settings()
+        threading.Thread(target=open_settings, args=()).start()
+
+def update_statistics(successful_pings, packet_loss, response_times):
+    if successful_pings > 0:
+        average_response_time = sum(response_times) / successful_pings
+        min_response_time = min(response_times)
+        max_response_time = max(response_times)
+    else:
+        average_response_time = 0
+        min_response_time = 0
+        max_response_time = 0
+
+    packet_loss_percentage = (packet_loss / (successful_pings + packet_loss)) * 100
+    tooltip = (
+        f'[Packet statistics]\n'
+        f'Sent: {successful_pings + packet_loss}\n'
+        f'Lost: {packet_loss} ({packet_loss_percentage:.2f}%)\n'
+        f'Avg. RTT: {average_response_time:.2f}ms\n'
+        f'Min. RTT: {min_response_time:.2f}ms\n'
+        f'Max. RTT: {max_response_time:.2f}ms'
+    )
+    return tooltip
+
 #Define ping test
-def ping_test():        
+def ping_test(icon):        
     global settings_changed
+    global stop_ping_test
+    global tray_icon
     global ping_count
     global seconds_between_pings
     global time_until_lost
-    global server_to_ping
+    global server_to_ping        
+
     while not stop_ping_test:
+        #Reaload settings if modified
         if settings_changed:
             ping_count, seconds_between_pings, time_until_lost, server_to_ping = read_settings()
             settings_changed = False
+
+        # Initialize statistics variables                
         successful_pings = 0
         packet_loss = 0
-        total_response_time = 0
         response_times = []
-        response_time = 0  # set default value        
+        response_time = 0  
+
         #Run ping command x amount of times 3 times per second.
         for i in range(ping_count):
-            result = subprocess.run(['ping', '-n', '1', '-w', str(time_until_lost), server_to_ping], stdout=subprocess.PIPE, creationflags = subprocess.CREATE_NO_WINDOW)
+            print(threading.enumerate())
+            result = subprocess.run(['ping', '-n', '1', '-w', str(time_until_lost), server_to_ping], stdout=subprocess.PIPE, creationflags = subprocess.CREATE_NO_WINDOW)            
             if 'Reply from' in str(result.stdout):
                 successful_pings += 1
                 # extract the response time from the output
                 response_time = float(result.stdout.decode('utf-8').split('time=')[1].split('ms')[0])
-                total_response_time += response_time
                 response_times.append(response_time)
             else:
-                total_response_time += response_time
                 response_times.append(response_time)
                 packet_loss += 1            
             time.sleep(seconds_between_pings)
+        
 
+        # Update tooltip with newer statistick
         packet_loss_percentage = (packet_loss / ping_count) * 100
-
-        #Add packets data to the variables
-        if successful_pings > 0:
-            average_response_time = total_response_time / successful_pings
-            min_response_time = min(response_times)
-            max_response_time = max(response_times)
-        else:
-            average_response_time = 0
-            min_response_time = 0
-            max_response_time = 0
-
-        # update the tooltip of the system tray icon with the statistics
-        tooltip = f'[Packet statistics]\nSent: {successful_pings + packet_loss}\nLost: {packet_loss} ({packet_loss_percentage:.2f}%)\nAvg. RTT: {average_response_time:.2f}ms\nMin. RTT: {min_response_time:.2f}ms\nMax. RTT: {max_response_time:.2f}ms'
+        tooltip = update_statistics(successful_pings, packet_loss, response_times)
         tray_icon.title = tooltip
 
-        #Set the system tray icon depending on packet loss percentages
-        if packet_loss_percentage < 9:
+        #Set the system tray accordingly
+        if packet_loss_percentage < 9: 
             icon_change('Icons/Perfect.png')
         elif packet_loss_percentage < 18:
             icon_change('Icons/Good.png')
@@ -90,15 +107,7 @@ def ping_test():
             icon_change('Icons/Medium.png')
         else:
             icon_change('Icons/Bad.png')
-
-        #Restart Variables
-        packet_loss = 0
-        total_response_time = 0
-
-#Start the ping test thread
-def start_thread(icon):
-    t = threading.Thread(target=ping_test)
-    t.start()
+     
 
 #Reads the current settings from the Settings.txt file.
 def read_settings():
@@ -112,7 +121,16 @@ def read_settings():
 
     # Parse the settings
     ping_count = config.getint('Settings', 'ping_count')
-    seconds_between_pings = config.getfloat('Settings', 'seconds_between_pings')
+
+    try:
+        seconds_between_pings = config.getfloat('Settings', 'seconds_between_pings')
+    except ValueError:
+        # If an error occurs while parsing seconds_between_pings, set its value to 0.33
+        seconds_between_pings = 0.33
+        config.set('Settings', 'seconds_between_pings', str(seconds_between_pings))
+        with open('Settings.txt', 'w') as configfile:
+            config.write(configfile)
+
     time_until_lost = config.getint('Settings', 'time_until_lost')
     server_to_ping = config.get('Settings', 'server_to_ping')
 
@@ -120,30 +138,21 @@ def read_settings():
     return ping_count, seconds_between_pings, time_until_lost, server_to_ping
 
 
-# Set up system tray menu
+# Set up system tray icon and context menu
 menu = pystray.Menu(pystray.MenuItem('Settings', on_right_click_settings),
                     pystray.MenuItem('Exit', on_right_click))
-
-
-# Fill variables with settings.txt
-ping_count, seconds_between_pings, time_until_lost, server_to_ping = read_settings()
-
-# Set up system tray icon
 icon_path = os.path.join(sys._MEIPASS, 'Icons', 'Waiting.png') if hasattr(sys, '_MEIPASS') else 'Icons/Waiting.png'
 icon = Image.open(icon_path)
 tray_icon = pystray.Icon('Testing', icon, 'Testing...', menu=menu)
-tray_icon.max_tooltip_width = 300
+tray_icon.max_tooltip_width = 350
 
-# flag to signal the ping_test thread to exit the loop
-stop_ping_test = False
+# Start the ping Thread
+threading.Thread(target=ping_test, args=(tray_icon,)).start()
 
-# Start the system tray icon
-threading.Thread(target=start_thread, args=(tray_icon,)).start()
-
-# Run 
+# Run icon on main thread
 try:
-    tray_icon.run()  # Run the system tray icon loop
+    tray_icon.run()  # Run the system tray icon     
 except Exception as e:
     print(f"Unexpected error occurred: {e}")
 finally:
-    tray_icon.stop()  # Stop the system tray icon loop
+    tray_icon.stop()  # Stop the system tray icon
