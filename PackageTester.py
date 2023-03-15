e1a325ad022642c1d7142e2c326ff41ddb8f9993
GenaroDS <genarodistilio@gmail.com>
2023-03-14 22:46:29 -0300

    This newwer version create a Settings.txt if there isn't one, and stops the ping test thread while its runing if the app is closed

--- PackageTester.py
+++ PackageTester.py
@@ -1,6 +1,4 @@
-import ctypes
 import os
-import win32process
 import subprocess
 import time
 import threading
@@ -32,7 +30,7 @@ def on_right_click(icon, item):
         icon.stop()
 
 #Create a third thread when settings is opened
-def on_right_click_settings(icon, item):        
+def on_right_click_settings(icon, item):
         threading.Thread(target=open_settings, args=()).start()
 
 def update_statistics(successful_pings, packet_loss, response_times):
@@ -57,7 +55,7 @@ def update_statistics(successful_pings, packet_loss, response_times):
     return tooltip
 
 #Define ping test
-def ping_test(icon):        
+def ping_test(icon):
     global settings_changed
     global stop_ping_test
     global tray_icon
@@ -79,19 +77,20 @@ def ping_test(icon):
         response_time = 0  
 
         #Run ping command x amount of times 3 times per second.
-        for i in range(ping_count):
-            print(threading.enumerate())
+        i = 0
+        while i < ping_count and not stop_ping_test:
             result = subprocess.run(['ping', '-n', '1', '-w', str(time_until_lost), server_to_ping], stdout=subprocess.PIPE, creationflags = subprocess.CREATE_NO_WINDOW)            
             if 'Reply from' in str(result.stdout):
                 successful_pings += 1
-                # extract the response time from the output
                 response_time = float(result.stdout.decode('utf-8').split('time=')[1].split('ms')[0])
                 response_times.append(response_time)
             else:
                 response_times.append(response_time)
                 packet_loss += 1            
             time.sleep(seconds_between_pings)
-        
+            print(i)
+            i += 1
+        print( i , ping_count)
 
         # Update tooltip with newer statistick
         packet_loss_percentage = (packet_loss / ping_count) * 100
@@ -106,37 +105,27 @@ def ping_test(icon):
         elif packet_loss_percentage < 28:        
             icon_change('Icons/Medium.png')
         else:
-            icon_change('Icons/Bad.png')
-     
+            icon_change('Icons/Bad.png')     
 
 #Reads the current settings from the Settings.txt file.
 def read_settings():
     config = configparser.ConfigParser()
-    if hasattr(sys, '_MEIPASS'):
-        # If running from PyInstaller executable, use sys._MEIPASS to find Settings.txt
-        config.read(os.path.join(sys._MEIPASS, 'Settings.txt'))
-    else:
-        # If running from source code, use relative path to Settings.txt
-        config.read('Settings.txt')
-
-    # Parse the settings
-    ping_count = config.getint('Settings', 'ping_count')
-
-    try:
-        seconds_between_pings = config.getfloat('Settings', 'seconds_between_pings')
-    except ValueError:
-        # If an error occurs while parsing seconds_between_pings, set its value to 0.33
-        seconds_between_pings = 0.33
-        config.set('Settings', 'seconds_between_pings', str(seconds_between_pings))
-        with open('Settings.txt', 'w') as configfile:
-            config.write(configfile)
-
-    time_until_lost = config.getint('Settings', 'time_until_lost')
-    server_to_ping = config.get('Settings', 'server_to_ping')
-
-    # Return tuple
+    config_file = "Settings.txt"
+    if not os.path.exists(config_file):
+        config["Settings"] = {
+            "ping_count": "40",
+            "seconds_between_pings": "0.33",
+            "time_until_lost": "150",
+            "server_to_ping": "google.com"
+        }
+        with open(config_file, "w") as file:
+            config.write(file)
+    config.read(config_file)
+    ping_count = int(config["Settings"]["ping_count"])
+    seconds_between_pings = float(config["Settings"]["seconds_between_pings"])
+    time_until_lost = int(config["Settings"]["time_until_lost"])
+    server_to_ping = config["Settings"]["server_to_ping"]
     return ping_count, seconds_between_pings, time_until_lost, server_to_ping
-
 
 # Set up system tray icon and context menu
 menu = pystray.Menu(pystray.MenuItem('Settings', on_right_click_settings),
@@ -155,4 +144,6 @@ try:
 except Exception as e:
     print(f"Unexpected error occurred: {e}")
 finally:
-    tray_icon.stop()  # Stop the system tray icon
+    tray_icon.stop()  # Stop the system tray icon    
+    
+    
